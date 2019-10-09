8b1a54d3f1a8a7d471b393b9183d1b29523c3039
BQ <qian.brian@gmail.com>
2019-10-08 21:10:22 -0400

    adding typescript support

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
-const express = require('express');
-const next = require('next');
-const morgan = require('morgan');
+import express from 'express';
+import next from 'next';
+import morgan from 'morgan';
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
